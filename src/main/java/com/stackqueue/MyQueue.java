354295e9dd6d1b8b95c999c7669f4b012005f433
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-15 11:29:21 +0530

    Added:Created Queue

---
+++ src/main/java/com/stackqueue/MyQueue.java
@@ -0,0 +1,25 @@
+package com.stackqueue;
+
+public class MyQueue {
+    private MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    public void enqueue(INode myNode) {
+        myLinkedList.add(myNode);
+    }
+
+    public void printStack() {
+        myLinkedList.printMyNodes();
+    }
+
+    public INode peak() {
+        return myLinkedList.head;
+    }
+
+    public INode pop() {
+        return myLinkedList.popLast();
+    }
+}
