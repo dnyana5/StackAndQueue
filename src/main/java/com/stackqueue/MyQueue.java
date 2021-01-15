b949a59c7ad0e9425f284faed7d58a6d07eade16
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-15 11:37:20 +0530

    Added:Delete from queue

--- src/main/java/com/stackqueue/MyQueue.java
+++ src/main/java/com/stackqueue/MyQueue.java
@@ -22,4 +22,8 @@ public class MyQueue {
     public INode pop() {
         return myLinkedList.popLast();
     }
+
+    public INode dequeue() {
+            return myLinkedList.popLast();
+        }
 }
